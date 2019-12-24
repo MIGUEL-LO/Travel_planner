caefae8f745453dd9f11267ed1557a69867803d0
Miguel Lopez <miguel.birbuet.19@ucl.ac.uk>
2019-12-24 20:12:45 +0000

    Making the class for Route. There is some nan issues when reading the file, and affect the timetable, also the plot seems off for the route

---
+++ route_class.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import numpy as np
+from IPython.display import display
+import matplotlib.pyplot as plt
+
+class Route:
+    def __init__(self, filename):
+        self.filename = filename
+    
+    def read_route(self):
+        df = pd.read_csv(self.filename, names=['x1','y1','stop'])
+        # Make collapsed columns for positional dat
+        df['x1, y1, stop'] = list(zip(df['x1'],df['y1'],df['stop']))
+        # Make output list
+        data_out = [(df.iloc[i]['x1, y1, stop']) for i in range(len(df))]
+        return data_out
+
+    def plot_map(self): #filename):
+        route = self.read_route()
+        max_x = max([n[0] for n in route]) + 5 # adds padding
+        max_y = max([n[1] for n in route]) + 5
+        grid = np.zeros((max_y, max_x))
+        for x,y,stop in route:
+            grid[y, x] = 1
+            if stop:
+                grid[y, x] += 1
+        fig, ax = plt.subplots(1, 1)
+        ax.pcolor(grid)
+        ax.invert_yaxis()
+        ax.set_aspect('equal', 'datalim')
+        plt.show()
+        
+
+    # stops is a dictionary holding the bus stop name and time of arrival. 
+    # starting from 0 at stop A and taking 10 mins to reach checkpoints in  
+    # the travel of bus
+    def timetable(self):
+        '''
+        Generates a timetable for a route as minutes from its first stop.
+        '''
+        route1 = self.read_route()
+        time = 0
+        stops = {}
+        for step in route1:
+            if step[2]:
+                stops[step[2]] = time
+            time += 10
+        return stops
+
+if __name__ == "__main__":
+    obj = Route("route.csv")
+    print(obj.read_route())
+    print(obj.timetable())
+    print(obj.plot_map())
